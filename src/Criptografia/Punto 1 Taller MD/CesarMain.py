8595d10432a2917cd4e01bc61101bcdb478cfb2c
Miguel Sanchez <miguelss304@gmail.com>
2026-07-24 23:18:56 -0500

    Eliminar archivos de implementación de cifrado y descifrado, así como funciones relacionadas en el módulo de criptografía.

--- src/Criptografia/Punto 1 Taller MD/CesarMain.py
+++
@@ -1,8 +0,0 @@
-from Cifrado import cifrar_mensaje
-from Descifrado import descifrar_mensaje
-x = int(input("Ingrese si desea cifrar o descifrar el mensaje: \n 1. Cifrar \n 2. Descifrar \n"))
-if x == 1:
-    cifrar_mensaje()
-elif x == 2:
-    descifrar_mensaje()
-#Funciona como main para que el usuario escoja que desea
--- src/Shannon/Punto 9 taller MD/Probabilidad.py
+++
@@ -1,6 +0,0 @@
-def calcular_probabilidad(letras,texto):
-    longitud = len(texto) #Calculamos el total de los caracteres del texto
-    probabilidad = {} #Creamos un diccionario vacio para almacenar la probabilidad de cada letra 
-    for letra, numero_veces_aparece in letras.items(): #Como llamamos "letras.items()" nos devuelve la clave y el numero de veces que aparece, por lo cual para el for, necesitamos contener ambos valores
-        probabilidad[letra] = numero_veces_aparece / longitud #Dividimos el numero de veces que aparece la letra por la longitud del texto, asi obteniendo la probabilidad, y procedemos a actualizar el diccionario con la letra y su probabilidad
-    return probabilidad
